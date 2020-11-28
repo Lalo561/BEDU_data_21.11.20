2eaff357f5e1ffe243603b53ad5fe3f67e52e44d
Lalo561 <69480811+Lalo561@users.noreply.github.com>
2020-11-27 19:56:33 -0600

    1st commit

---
+++ 01_Listas.py
@@ -0,0 +1,18 @@
+#Archivos explicando listas
+
+numeros = [23,89,127,1289,8912]
+print(numeros)
+
+# Para agregar un elemento a una lista ocupamos ".append"
+
+numero_extra = 33
+numero_extra_dos = 1233
+numero_extra_tres = 89
+
+numeros.append(numero_extra)
+
+# Con ".pop" eliminamos el último elemento de la lista
+
+numeros.pop()
+
+print(numeros)
---
+++ README.md
@@ -0,0 +1,4 @@
+# BEDU DATA
+### SESION 02
+
+Ejemplos realizados en la clase número 2 en Bedu.org.
